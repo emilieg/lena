a91e72666edf47988c5abd9c9ef9e5b57379ad4d
emilieg <emilie.gerber@hotmail.com>
2016-11-10 14:20:40 -0800

    added code to serve different nav logo on mobile screens

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     $(document).foundation();
+    if($(window).width() < 639) {
+      console.log("in mobile view", $(window).width());
+     //change the navigation logo to a smaller version
+    $('.nav-logo').attr('src', 'assets/LY-Lotus.png');
+    }
 });
 
-// var elem = new Foundation.Orbit(element, options);
